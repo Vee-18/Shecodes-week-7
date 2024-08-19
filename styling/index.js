e3edd78667d4d2930dea2cc863a3eb8cdf1f927c
Vee <veee18@proton.me>
2024-08-18 18:56:10 -0600

    updated API key

--- styling/index.js
+++ styling/index.js
@@ -8,8 +8,8 @@ function refreshWeather(response) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  let apiKey = "cac43047defea2otdb4a969fb0cf90a7";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={key}`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
